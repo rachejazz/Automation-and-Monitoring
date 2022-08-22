5115d2f42b7ab6e83cb35ae4be79f2f53c7d1039
ws-divya <divya.goswami@workspan.com>
2022-08-23 04:29:59 +0530

    removed binaries
    Signed-off-by: ws-divya <divya.goswami@workspan.com>

--- instances.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "|KeyName|LaunchedOn|Tags|" > ec2instances.md
-echo "|-|-|-|" >> ec2instances.md
-
-aws ec2 describe-instances | jq  -r '{"Reservations"}|.[]|.[].Instances|.[] | "\(.KeyName)|\(.LaunchTime)|\(.Tags)"' >> ec2instances.md
-
-
